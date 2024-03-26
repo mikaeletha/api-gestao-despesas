c88d109d43b1ba0b068f5235122c91bad72ac9e7
Tulio <tulio.ribeiromaia@gmail.com>
2024-03-26 20:36:27 -0300

    Add DB config, dependencies of Entity Framework and Migrations

---
+++ Controllers/ExpensesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api_gestao_despesas.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
