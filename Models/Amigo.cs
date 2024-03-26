96293f713578dd6d3d22e6d47cca328adf440493
Isabela Bersan <bersanisabela@gmail.com>
2024-03-26 20:47:41 -0300

    Criação do Modelos de Amigo

---
+++ Models/Amigo.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api_gestao_despesas.Models
+{
+    [Table("Amigos")]
+    public class Amigo
+    {
+        [Key]
+        public int Id_amigo { get; set; }
+        [Required]
+        public string CPF { get; set; }
+
+    }
+}
